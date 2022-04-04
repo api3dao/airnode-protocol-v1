5b6bb2845acb51054c7f38313019da0e069df9e5
bbenligiray <bbenligiray@gmail.com>
2022-04-04 20:50:42 +0300

    Inherit Multicall at AccessControlRegistryAdminned level

--- contracts/allocators/Allocator.sol
+++ contracts/allocators/Allocator.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Multicall.sol";
 import "./interfaces/IAllocator.sol";
 
 /// @title Abstract contract to be inherited by Allocator contracts that
@@ -10,7 +9,7 @@ import "./interfaces/IAllocator.sol";
 /// of slots to serve the respective subscriptions. What these Allocators and
 /// slot numbers are expected to be communicated off-chain. The Airnode/relayer
 /// should not process expired slots or subscriptions with invalid IDs.
-abstract contract Allocator is Multicall, IAllocator {
+abstract contract Allocator is IAllocator {
     struct Slot {
         bytes32 subscriptionId;
         address setter;
