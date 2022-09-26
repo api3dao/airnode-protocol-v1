017285dd7fcbd6d343eef8fbdc5730b2487d1258
bbenligiray <bbenligiray@gmail.com>
2022-09-26 21:36:46 +0300

    Move IDapiProxy

--- contracts/dapis/proxies/DataFeedProxy.sol
+++ contracts/dapis/proxies/DataFeedProxy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../interfaces/IDapiProxy.sol";
+import "./interfaces/IDapiProxy.sol";
 
 contract DataFeedProxy is IDapiProxy {
     address public immutable dapiServer;
--- contracts/dapis/proxies/interfaces/IDapiProxy.sol
+++ contracts/dapis/proxies/interfaces/IDapiProxy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./IDapiServer.sol";
+import "../../interfaces/IDapiServer.sol";
 
 interface IDapiProxy {
     function read() external view returns (int224 value, uint32 timestamp);
