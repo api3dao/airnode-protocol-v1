ad5b935af5a1237cf926711207e2c0afd8d7c7f4
bbenligiray <bbenligiray@gmail.com>
2022-09-26 18:44:43 +0300

    Implement DataFeedProxy

---
+++ contracts/dapis/DataFeedProxy.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./interfaces/IDapiServer.sol";
+
+contract DataFeedProxy {
+    address public immutable dapiServer;
+    bytes32 public immutable dataFeedIdOrDapiNameHash;
+    string public metadata;
+
+    constructor(
+        address _dapiServer,
+        bytes32 _dataFeedIdOrDapiNameHash,
+        string memory _metadata
+    ) {
+        dapiServer = _dapiServer;
+        dataFeedIdOrDapiNameHash = _dataFeedIdOrDapiNameHash;
+        metadata = _metadata; // Includes policy hash
+    }
+
+    function readDataFeed()
+        external
+        view
+        virtual
+        returns (int224 value, uint32 timestamp)
+    {
+        return IDapiServer(dapiServer).readDataFeed(dataFeedIdOrDapiNameHash);
+    }
+}
