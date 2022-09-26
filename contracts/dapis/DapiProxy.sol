877a9cb6a94dc35001ff7b9952e8c1cb1a59a82e
bbenligiray <bbenligiray@gmail.com>
2022-09-26 18:57:34 +0300

    Have DapiProxy have policyHash instead of the generic metadata

--- contracts/dapis/DapiProxy.sol
+++ contracts/dapis/DapiProxy.sol
@@ -6,16 +6,16 @@ import "./interfaces/IDapiServer.sol";
 contract DapiProxy {
     address public immutable dapiServer;
     bytes32 public immutable dataFeedIdOrDapiNameHash;
-    string public metadata;
+    bytes32 public immutable policyHash;
 
     constructor(
         address _dapiServer,
         bytes32 _dataFeedIdOrDapiNameHash,
-        string memory _metadata
+        bytes32 _policyHash
     ) {
         dapiServer = _dapiServer;
         dataFeedIdOrDapiNameHash = _dataFeedIdOrDapiNameHash;
-        metadata = _metadata; // Includes policy hash
+        policyHash = _policyHash;
     }
 
     function read()
