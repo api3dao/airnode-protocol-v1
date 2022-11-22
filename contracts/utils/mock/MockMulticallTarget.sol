f8feb8ab9e15a3dc138c62d3d5abdd1c8f02cfb8
bbenligiray <bbenligiray@gmail.com>
2022-11-22 11:44:01 +0300

    Add the pure modifier to the mock contract

--- contracts/utils/mock/MockMulticallTarget.sol
+++ contracts/utils/mock/MockMulticallTarget.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
+/// @dev The multicall target functions are pure for simplicity. This does not
+/// necessarily have to be the case in practice.
 contract MockMulticallTarget {
     error MyError(uint256 fieldAlways123, string fieldAlwaysFoo);
 
@@ -14,20 +16,22 @@ contract MockMulticallTarget {
     function alwaysRevertsWithCustomError(
         int256 argPositive,
         int256 argNegative
-    ) external {
+    ) external pure {
         require(argPositive > 0 && argNegative < 0, "Invalid argument");
         revert MyError(123, "Foo");
     }
 
     function alwaysRevertsWithNoData(int256 argPositive, int256 argNegative)
         external
+        pure
     {
         require(argPositive > 0 && argNegative < 0, "Invalid argument");
-        revert();
+        revert(); // solhint-disable-line reason-string
     }
 
     function convertsPositiveArgumentToNegative(int256 argPositive)
         external
+        pure
         returns (int256)
     {
         require(argPositive > 0, "Argument not positive");
