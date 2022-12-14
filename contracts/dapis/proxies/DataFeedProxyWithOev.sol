fbbf6d46c5561d780b78f670251490aeca6cc4d0
bbenligiray <bbenligiray@gmail.com>
2022-12-14 11:59:35 +0300

    Add proxy docstrings

--- contracts/dapis/proxies/DataFeedProxyWithOev.sol
+++ contracts/dapis/proxies/DataFeedProxyWithOev.sol
@@ -4,11 +4,34 @@ pragma solidity ^0.8.0;
 import "./interfaces/IDataFeedProxy.sol";
 import "./interfaces/IOevUpdater.sol";
 
+/// @title An immutable proxy contract that is used to read a specific data
+/// feed (Beacon or Beacon set) of a specific DapiServer contract, execute OEV
+/// updates and let the beneficiary withdraw the accumulated proceeds
+/// @dev The proxy contracts are generalized to support most types of numerical
+/// data feeds. This means that the user of this proxy is expected to validate
+/// the read values according to the specific use-case. For example, `value` is
+/// a signed integer, yet it being negative may not make sense in the case that
+/// the data feed represents the spot price of an asset. In that case, the user
+/// is responsible with ensuring that `value` is not negative.
+/// `timestamp` is derived from the system times of the Airnodes that signed
+/// the data that contributed to the most recent update (which is not equal to
+/// the block time of the most recent update). Its main function is to prevent
+/// out of date values from being used to update data feeds. If you will be
+/// implementing a contract that uses `timestamp` in the contract logic in any
+/// way (e.g., reject readings with `timestamp` that is more than 1 day old),
+/// make sure to refer to DapiServer.sol and understand how this number is
+/// derived.
 contract DataFeedProxyWithOev is IDataFeedProxy, IOevUpdater {
+    /// @notice DapiServer address
     address public immutable override dapiServer;
+    /// @notice Data feed ID
     bytes32 public immutable override dataFeedId;
+    /// @notice OEV beneficiary address
     address public immutable override oevBeneficiary;
 
+    /// @param _dapiServer DapiServer address
+    /// @param _dataFeedId Data feed (Beacon or Beacon set) ID
+    /// @param _oevBeneficiary OEV beneficiary
     constructor(
         address _dapiServer,
         bytes32 _dataFeedId,
@@ -19,6 +42,8 @@ contract DataFeedProxyWithOev is IDataFeedProxy, IOevUpdater {
         oevBeneficiary = _oevBeneficiary;
     }
 
+    /// @notice Called by anyone to withdraw the OEV proceeds to the
+    /// beneficiary account
     function withdraw() external {
         uint256 balance = address(this).balance;
         require(balance > 0, "Zero balance");
@@ -27,16 +52,28 @@ contract DataFeedProxyWithOev is IDataFeedProxy, IOevUpdater {
         require(success, "Beneficiary reverted withdrawal");
     }
 
+    /// @notice Called by the OEV auction winner along with the bid payment to
+    /// update the OEV proxy Beacon
+    /// @dev The winner of the auction calls this in a `multicall()` and
+    /// extracts the OEV in subsequent calls of the same transaction
+    /// @param airnode Airnode address
+    /// @param templateId Template ID
+    /// @param timestamp Timestamp used in the signature
+    /// @param data Response data (an `int256` encoded in contract ABI)
+    /// @param expirationTimestamp Expiration timestamp of the signature
+    /// @param bidAmount Amount of the bid that won the OEV auction
+    /// @param signature Template ID, a timestamp and the response data signed
+    /// for the specific bid by the Airnode address
     function updateOevProxyBeaconWithSignedData(
         address airnode,
         bytes32 templateId,
         uint256 timestamp,
         bytes memory data,
-        uint256 expireTimestamp,
+        uint256 expirationTimestamp,
         uint256 bidAmount,
         bytes memory signature
     ) external payable {
-        require(block.timestamp < expireTimestamp, "Expired signature");
+        require(block.timestamp < expirationTimestamp, "Expired signature");
         require(msg.value == bidAmount, "Invalid bid amount");
         IDapiServer(dapiServer).updateOevProxyBeaconWithSignedData(
             airnode,
@@ -47,23 +84,36 @@ contract DataFeedProxyWithOev is IDataFeedProxy, IOevUpdater {
                 block.chainid,
                 address(this),
                 msg.sender,
-                expireTimestamp,
+                expirationTimestamp,
                 bidAmount
             ),
             signature
         );
     }
 
+    /// @notice Called by the OEV auction winner along with the bid payment to
+    /// update the OEV proxy Beacon set
+    /// @dev The winner of the auction calls this in a `multicall()` and
+    /// extracts the OEV in subsequent calls of the same transaction
+    /// @param airnodes Airnode addresses
+    /// @param templateIds Template IDs
+    /// @param timestamps Timestamps used in the signatures
+    /// @param data Response data (an `int256` encoded in contract ABI per
+    /// Beacon)
+    /// @param expirationTimestamp Expiration timestamp of the signatures
+    /// @param bidAmount Amount of the bid that won the OEV auction
+    /// @param signatures Template ID, a timestamp and the response data signed
+    /// for the specific bid by the respective Airnode address per Beacon
     function updateOevProxyBeaconSetWithSignedData(
         address[] memory airnodes,
         bytes32[] memory templateIds,
         uint256[] memory timestamps,
         bytes[] memory data,
-        uint256 expireTimestamp,
+        uint256 expirationTimestamp,
         uint256 bidAmount,
         bytes[] memory signatures
     ) external payable {
-        require(block.timestamp < expireTimestamp, "Expired signature");
+        require(block.timestamp < expirationTimestamp, "Expired signature");
         require(msg.value == bidAmount, "Invalid bid amount");
         IDapiServer(dapiServer).updateOevProxyBeaconSetWithSignedData(
             airnodes,
@@ -74,13 +124,16 @@ contract DataFeedProxyWithOev is IDataFeedProxy, IOevUpdater {
                 block.chainid,
                 address(this),
                 msg.sender,
-                expireTimestamp,
+                expirationTimestamp,
                 bidAmount
             ),
             signatures
         );
     }
 
+    /// @notice Reads the data feed that this proxy maps to
+    /// @return value Data feed value
+    /// @return timestamp Data feed timestamp
     function read()
         external
         view
