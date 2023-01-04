e1ec9a1824ef6abe54e32e0a90f6953fc8aaf07d
bbenligiray <bbenligiray@gmail.com>
2023-01-04 14:43:27 +0300

    Fix docstrings

--- contracts/utils/OwnableCallForwarder.sol
+++ contracts/utils/OwnableCallForwarder.sol
@@ -6,14 +6,12 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "./interfaces/IOwnableCallForwarder.sol";
 
 /// @title Contract that forwards the calls that its owner sends
-/// @dev AccessControlRegistry users that want their access control tables
+/// @notice AccessControlRegistry users that want their access control tables
 /// to be transferrable (e.g., a DAO) will use this forwarder instead of
 /// interacting with it directly. There are cases where this transferrability
 /// is not desired, e.g., if the user is an Airnode and is immutably associated
 /// with a single address, in which case the manager will interact with
 /// AccessControlRegistry directly.
-/// The ownership of this contract is deliberately renouncable. If this does
-/// suit the use case, override and disable this functionality.
 contract OwnableCallForwarder is Ownable, IOwnableCallForwarder {
     /// @notice Forwards the calldata and the value to the target address if
     /// the sender is the owner and returns the data
