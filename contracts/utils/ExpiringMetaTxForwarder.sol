b1bd25cf54d6f90712ee2597f44545cdc7076189
bbenligiray <bbenligiray@gmail.com>
2023-01-04 14:33:47 +0300

    Specify Solidity versions for contracts that will be deployed in standalone form

--- contracts/utils/ExpiringMetaTxForwarder.sol
+++ contracts/utils/ExpiringMetaTxForwarder.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
