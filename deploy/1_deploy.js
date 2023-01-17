2adfd6d812047f9dcc633e7835da5e005515345c
bbenligiray <bbenligiray@gmail.com>
2023-01-17 17:37:35 +0300

    Update deployment scripts to deploy all contracts required for ProxyFactory

--- deploy/1_deploy.js
+++ deploy/1_deploy.js
@@ -1,8 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 const hre = require('hardhat');
 
 module.exports = async ({ getUnnamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const accounts = await getUnnamedAccounts();
+
+  const accessControlRegistry = await deploy('AccessControlRegistry', {
+    from: accounts[0],
+    log: true,
+    deterministicDeployment: process.env.DETERMINISTIC ? hre.ethers.constants.HashZero : undefined,
+  });
+  log(`Deployed AccessControlRegistry at ${accessControlRegistry.address}`);
 
   const airnodeProtocol = await deploy('AirnodeProtocol', {
     from: accounts[0],
@@ -10,5 +19,25 @@ module.exports = async ({ getUnnamedAccounts, deployments }) => {
     deterministicDeployment: process.env.DETERMINISTIC ? hre.ethers.constants.HashZero : undefined,
   });
   log(`Deployed AirnodeProtocol at ${airnodeProtocol.address}`);
+
+  const adminRoleDescription = 'DapiServer admin';
+  const managerAddress = JSON.parse(fs.readFileSync(path.join('manager-multisig', 'deployments', 'references.json')))[
+    hre.network.name
+  ].manager;
+  const dapiServer = await deploy('DapiServer', {
+    from: accounts[0],
+    args: [accessControlRegistry.address, adminRoleDescription, managerAddress, airnodeProtocol.address],
+    log: true,
+    deterministicDeployment: process.env.DETERMINISTIC ? hre.ethers.constants.HashZero : undefined,
+  });
+  log(`Deployed DapiServer at ${dapiServer.address}`);
+
+  const proxyFactory = await deploy('ProxyFactory', {
+    from: accounts[0],
+    args: [dapiServer.address],
+    log: true,
+    deterministicDeployment: process.env.DETERMINISTIC ? hre.ethers.constants.HashZero : undefined,
+  });
+  log(`Deployed ProxyFactory at ${proxyFactory.address}`);
 };
 module.exports.tags = ['deploy'];
--- deploy/2_document.js
+++ deploy/2_document.js
@@ -7,19 +7,25 @@ module.exports = async () => {
     .readdirSync('deployments', { withFileTypes: true })
     .filter((item) => item.isDirectory())
     .map((item) => item.name);
-  const contractNames = ['AirnodeProtocol'];
-  const references = {};
-  references.chainNames = {};
-  for (const network of networks) {
-    references.chainNames[hre.config.networks[network].chainId] = network;
-  }
-  for (const contractName of contractNames) {
-    references[contractName] = {};
-    for (const network of networks) {
-      const deployment = JSON.parse(fs.readFileSync(path.join('deployments', network, `${contractName}.json`), 'utf8'));
-      references[contractName][hre.config.networks[network].chainId] = deployment.address;
-    }
-  }
+  const contractNames = ['AccessControlRegistry', 'AirnodeProtocol', 'DapiServer', 'ProxyFactory'];
+
+  const references = {
+    chainNames: networks.reduce((acc, network) => {
+      return { ...acc, [hre.config.networks[network].chainId]: network };
+    }, {}),
+    contracts: {
+      ...contractNames.reduce((acc, contractName) => {
+        return {
+          ...acc,
+          [contractName]: networks.reduce((acc, network) => {
+            const deployment = JSON.parse(
+              fs.readFileSync(path.join('deployments', network, `${contractName}.json`), 'utf8')
+            );
+            return { ...acc, [hre.config.networks[network].chainId]: deployment.address };
+          }, {}),
+        };
+      }, {}),
+    },
+  };
   fs.writeFileSync(path.join('deployments', 'references.json'), JSON.stringify(references, null, 2));
 };
-module.exports.tags = ['document'];
