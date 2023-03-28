2a656c73e2b411534e1f2f8d04c2478fea88aafb
bbenligiray <bbenligiray@gmail.com>
2023-03-28 17:13:31 +0300

    Update deployment script

--- deploy/1_deploy.js
+++ deploy/1_deploy.js
@@ -1,54 +1,48 @@
-const { ethers, network } = require('hardhat');
+const hre = require('hardhat');
 const managerMultisigAddresses = require('../deployments/manager-multisig.json');
 
 module.exports = async ({ getUnnamedAccounts, deployments }) => {
-  const { deploy, log } = deployments;
+  const zk = require('zksync-web3');
+  const { Deployer } = require('@matterlabs/hardhat-zksync-deploy');
+  const zkWallet = zk.Wallet.fromMnemonic(hre.config.networks[hre.network.name].accounts.mnemonic);
+  const deployer = new Deployer(hre, zkWallet);
+
+  const { log } = deployments;
   const accounts = await getUnnamedAccounts();
 
-  const accessControlRegistry = await deploy('AccessControlRegistry', {
-    from: accounts[0],
-    log: true,
-    deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
-  });
+  const AccessControlRegistry = await deployer.loadArtifact('AccessControlRegistry');
+  const accessControlRegistry = await deployer.deploy(AccessControlRegistry, []);
   log(`Deployed AccessControlRegistry at ${accessControlRegistry.address}`);
 
-  const { address: ownableCallForwarderAddress, abi: ownableCallForwarderAbi } = await deploy('OwnableCallForwarder', {
-    from: accounts[0],
-    args: [accounts[0]],
-    log: true,
-    deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
-  });
+  const OwnableCallForwarder = await deployer.loadArtifact('OwnableCallForwarder');
+  const { address: ownableCallForwarderAddress } = await deployer.deploy(OwnableCallForwarder, [accounts[0]]);
   log(`Deployed OwnableCallForwarder at ${ownableCallForwarderAddress}`);
 
-  const ownableCallForwarder = new ethers.Contract(
+  const ownableCallForwarder = new hre.ethers.Contract(
     ownableCallForwarderAddress,
-    ownableCallForwarderAbi,
-    (await ethers.getSigners())[0]
+    OwnableCallForwarder.abi,
+    (await hre.ethers.getSigners())[0]
   );
   if ((await ownableCallForwarder.owner()) === accounts[0]) {
-    const receipt = await ownableCallForwarder.transferOwnership(managerMultisigAddresses[network.name]);
+    const receipt = await ownableCallForwarder.transferOwnership(managerMultisigAddresses[hre.network.name]);
     await new Promise((resolve) =>
-      ethers.provider.once(receipt.hash, () => {
+      hre.ethers.provider.once(receipt.hash, () => {
         resolve();
       })
     );
-    log(`Transferred OwnableCallForwarder ownership to ${managerMultisigAddresses[network.name]}`);
+    log(`Transferred OwnableCallForwarder ownership to ${managerMultisigAddresses[hre.network.name]}`);
   }
 
-  const api3ServerV1 = await deploy('Api3ServerV1', {
-    from: accounts[0],
-    args: [accessControlRegistry.address, 'Api3ServerV1 admin', ownableCallForwarder.address],
-    log: true,
-    deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
-  });
+  const Api3ServerV1 = await deployer.loadArtifact('Api3ServerV1');
+  const api3ServerV1 = await deployer.deploy(Api3ServerV1, [
+    accessControlRegistry.address,
+    'Api3ServerV1 admin',
+    ownableCallForwarder.address,
+  ]);
   log(`Deployed Api3ServerV1 at ${api3ServerV1.address}`);
 
-  const proxyFactory = await deploy('ProxyFactory', {
-    from: accounts[0],
-    args: [api3ServerV1.address],
-    log: true,
-    deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
-  });
+  const ProxyFactory = await deployer.loadArtifact('ProxyFactory');
+  const proxyFactory = await deployer.deploy(ProxyFactory, [api3ServerV1.address]);
   log(`Deployed ProxyFactory at ${proxyFactory.address}`);
 };
 module.exports.tags = ['deploy'];
