df29062cae3db44def7a168270820b14221bf7d1
bbenligiray <bbenligiray@gmail.com>
2023-05-02 15:51:19 +0300

    Add mock ERC2612 token contract

---
+++ contracts/utils/mock/MockToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+contract MockToken is ERC20Permit {
+    constructor() ERC20("Token", "TKN") ERC20Permit("Token") {
+        _mint(msg.sender, 1e6 * 10 ** decimals());
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+}
