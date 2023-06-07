f7271e715a5b82f2b276bb4fbd1b013ba12b119f
bbenligiray <bbenligiray@gmail.com>
2023-06-07 16:49:29 +0300

    Refactor deployment scripts

--- deploy/1_deploy.js
+++ deploy/1_deploy.js
@@ -3,6 +3,7 @@ const managerMultisigAddresses = require('../deployments/manager-multisig.json')
 const {
   chainsSupportedByApi3Market,
   chainsSupportedByChainApi,
+  chainsSupportedByDapis,
   chainsSupportedByOevRelay,
 } = require('../src/supported-chains');
 
@@ -10,86 +11,94 @@ module.exports = async ({ getUnnamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const accounts = await getUnnamedAccounts();
 
-  const accessControlRegistry = await deploy('AccessControlRegistry', {
-    from: accounts[0],
-    log: true,
-    deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
-  });
-  log(`Deployed AccessControlRegistry at ${accessControlRegistry.address}`);
-
-  const { address: ownableCallForwarderAddress, abi: ownableCallForwarderAbi } = await deploy('OwnableCallForwarder', {
-    from: accounts[0],
-    args: [accounts[0]],
-    log: true,
-    deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
-  });
-  log(`Deployed OwnableCallForwarder at ${ownableCallForwarderAddress}`);
-
-  const ownableCallForwarder = new ethers.Contract(
-    ownableCallForwarderAddress,
-    ownableCallForwarderAbi,
-    (await ethers.getSigners())[0]
-  );
-  if ((await ownableCallForwarder.owner()) === accounts[0]) {
-    const receipt = await ownableCallForwarder.transferOwnership(managerMultisigAddresses[network.name]);
-    await new Promise((resolve) =>
-      ethers.provider.once(receipt.hash, () => {
-        resolve();
-      })
-    );
-    log(`Transferred OwnableCallForwarder ownership to ${managerMultisigAddresses[network.name]}`);
-  }
-
-  const api3ServerV1 = await deploy('Api3ServerV1', {
-    from: accounts[0],
-    args: [accessControlRegistry.address, 'Api3ServerV1 admin', ownableCallForwarder.address],
-    log: true,
-    deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
-  });
-  log(`Deployed Api3ServerV1 at ${api3ServerV1.address}`);
-
-  const proxyFactory = await deploy('ProxyFactory', {
-    from: accounts[0],
-    args: [api3ServerV1.address],
-    log: true,
-    deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
-  });
-  log(`Deployed ProxyFactory at ${proxyFactory.address}`);
-
-  if (chainsSupportedByOevRelay.includes(network.name)) {
-    const usdcAddress = { ethereum: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48' };
-    const prepaymentDepository = await deploy('PrepaymentDepository', {
+  if (chainsSupportedByDapis.includes(network.name) || chainsSupportedByChainApi.includes(network.name)) {
+    const accessControlRegistry = await deploy('AccessControlRegistry', {
       from: accounts[0],
-      args: [
-        accessControlRegistry.address,
-        'PrepaymentDepository admin (OEV Relay)',
-        ownableCallForwarder.address,
-        usdcAddress[network.name],
-      ],
       log: true,
       deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
     });
-    log(`Deployed PrepaymentDepository (OEV Relay) at ${prepaymentDepository.address}`);
-  }
+    log(`Deployed AccessControlRegistry at ${accessControlRegistry.address}`);
 
-  if (chainsSupportedByChainApi.includes(network.name) || network.name === 'ethereum-sepolia-testnet') {
-    const requesterAuthorizerWithErc721 = await deploy('RequesterAuthorizerWithErc721', {
-      from: accounts[0],
-      args: [accessControlRegistry.address, 'RequesterAuthorizerWithErc721 admin'],
-      log: true,
-      deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
-    });
-    log(`Deployed RequesterAuthorizerWithErc721 at ${requesterAuthorizerWithErc721.address}`);
-  }
+    if (chainsSupportedByDapis.includes(network.name)) {
+      const { address: ownableCallForwarderAddress, abi: ownableCallForwarderAbi } = await deploy(
+        'OwnableCallForwarder',
+        {
+          from: accounts[0],
+          args: [accounts[0]],
+          log: true,
+          deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
+        }
+      );
+      log(`Deployed OwnableCallForwarder at ${ownableCallForwarderAddress}`);
 
-  if (chainsSupportedByApi3Market.includes(network.name)) {
-    const orderPayable = await deploy('OrderPayable', {
-      from: accounts[0],
-      args: [accessControlRegistry.address, 'OrderPayable admin (API3 Market)', ownableCallForwarder.address],
-      log: true,
-      deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
-    });
-    log(`Deployed OrderPayable (API3 Market) at ${orderPayable.address}`);
+      const ownableCallForwarder = new ethers.Contract(
+        ownableCallForwarderAddress,
+        ownableCallForwarderAbi,
+        (await ethers.getSigners())[0]
+      );
+      if ((await ownableCallForwarder.owner()) === accounts[0]) {
+        const receipt = await ownableCallForwarder.transferOwnership(managerMultisigAddresses[network.name]);
+        await new Promise((resolve) =>
+          ethers.provider.once(receipt.hash, () => {
+            resolve();
+          })
+        );
+        log(`Transferred OwnableCallForwarder ownership to ${managerMultisigAddresses[network.name]}`);
+      }
+
+      const api3ServerV1 = await deploy('Api3ServerV1', {
+        from: accounts[0],
+        args: [accessControlRegistry.address, 'Api3ServerV1 admin', ownableCallForwarder.address],
+        log: true,
+        deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
+      });
+      log(`Deployed Api3ServerV1 at ${api3ServerV1.address}`);
+
+      const proxyFactory = await deploy('ProxyFactory', {
+        from: accounts[0],
+        args: [api3ServerV1.address],
+        log: true,
+        deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
+      });
+      log(`Deployed ProxyFactory at ${proxyFactory.address}`);
+
+      if (chainsSupportedByOevRelay.includes(network.name)) {
+        const usdcAddress = { ethereum: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48' };
+        const prepaymentDepository = await deploy('PrepaymentDepository', {
+          from: accounts[0],
+          args: [
+            accessControlRegistry.address,
+            'PrepaymentDepository admin (OEV Relay)',
+            ownableCallForwarder.address,
+            usdcAddress[network.name],
+          ],
+          log: true,
+          deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
+        });
+        log(`Deployed PrepaymentDepository (OEV Relay) at ${prepaymentDepository.address}`);
+      }
+
+      if (chainsSupportedByApi3Market.includes(network.name)) {
+        const orderPayable = await deploy('OrderPayable', {
+          from: accounts[0],
+          args: [accessControlRegistry.address, 'OrderPayable admin (API3 Market)', ownableCallForwarder.address],
+          log: true,
+          deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
+        });
+        log(`Deployed OrderPayable (API3 Market) at ${orderPayable.address}`);
+      }
+    }
+    if (chainsSupportedByChainApi.includes(network.name) || network.name === 'ethereum-sepolia-testnet') {
+      const requesterAuthorizerWithErc721 = await deploy('RequesterAuthorizerWithErc721', {
+        from: accounts[0],
+        args: [accessControlRegistry.address, 'RequesterAuthorizerWithErc721 admin'],
+        log: true,
+        deterministicDeployment: process.env.DETERMINISTIC ? ethers.constants.HashZero : undefined,
+      });
+      log(`Deployed RequesterAuthorizerWithErc721 at ${requesterAuthorizerWithErc721.address}`);
+    }
+  } else {
+    throw new Error(`${network.name} is not supported`);
   }
 };
 module.exports.tags = ['deploy'];
--- deploy/3_verify.js
+++ deploy/3_verify.js
@@ -1,68 +1,75 @@
 const hre = require('hardhat');
+const { network } = require('hardhat');
 const {
   chainsSupportedByApi3Market,
   chainsSupportedByChainApi,
+  chainsSupportedByDapis,
   chainsSupportedByOevRelay,
 } = require('../src/supported-chains');
 
 module.exports = async ({ getUnnamedAccounts, deployments }) => {
   const accounts = await getUnnamedAccounts();
 
-  const AccessControlRegistry = await deployments.get('AccessControlRegistry');
-  await hre.run('verify:verify', {
-    address: AccessControlRegistry.address,
-  });
-
-  const OwnableCallForwarder = await deployments.get('OwnableCallForwarder');
-  await hre.run('verify:verify', {
-    address: OwnableCallForwarder.address,
-    constructorArguments: [accounts[0]],
-  });
-
-  const Api3ServerV1 = await deployments.get('Api3ServerV1');
-  await hre.run('verify:verify', {
-    address: Api3ServerV1.address,
-    constructorArguments: [AccessControlRegistry.address, 'Api3ServerV1 admin', OwnableCallForwarder.address],
-  });
-
-  const ProxyFactory = await deployments.get('ProxyFactory');
-  await hre.run('verify:verify', {
-    address: ProxyFactory.address,
-    constructorArguments: [Api3ServerV1.address],
-  });
-
-  if (chainsSupportedByOevRelay.includes(hre.network.name)) {
-    const usdcAddress = { ethereum: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48' };
-    const PrepaymentDepository = await deployments.get('PrepaymentDepository');
+  if (chainsSupportedByDapis.includes(network.name) || chainsSupportedByChainApi.includes(network.name)) {
+    const AccessControlRegistry = await deployments.get('AccessControlRegistry');
     await hre.run('verify:verify', {
-      address: PrepaymentDepository.address,
-      constructorArguments: [
-        AccessControlRegistry.address,
-        'PrepaymentDepository admin (OEV Relay)',
-        OwnableCallForwarder.address,
-        usdcAddress[hre.network.name],
-      ],
+      address: AccessControlRegistry.address,
     });
-  }
 
-  if (chainsSupportedByChainApi.includes(hre.network.name) || hre.network.name === 'ethereum-sepolia-testnet') {
-    const RequesterAuthorizerWithErc721 = await deployments.get('RequesterAuthorizerWithErc721');
-    await hre.run('verify:verify', {
-      address: RequesterAuthorizerWithErc721.address,
-      constructorArguments: [AccessControlRegistry.address, 'RequesterAuthorizerWithErc721 admin'],
-    });
-  }
+    if (chainsSupportedByDapis.includes(network.name)) {
+      const OwnableCallForwarder = await deployments.get('OwnableCallForwarder');
+      await hre.run('verify:verify', {
+        address: OwnableCallForwarder.address,
+        constructorArguments: [accounts[0]],
+      });
 
-  if (chainsSupportedByApi3Market.includes(hre.network.name)) {
-    const OrderPayable = await deployments.get('OrderPayable');
-    await hre.run('verify:verify', {
-      address: OrderPayable.address,
-      constructorArguments: [
-        AccessControlRegistry.address,
-        'OrderPayable admin (API3 Market)',
-        OwnableCallForwarder.address,
-      ],
-    });
+      const Api3ServerV1 = await deployments.get('Api3ServerV1');
+      await hre.run('verify:verify', {
+        address: Api3ServerV1.address,
+        constructorArguments: [AccessControlRegistry.address, 'Api3ServerV1 admin', OwnableCallForwarder.address],
+      });
+
+      const ProxyFactory = await deployments.get('ProxyFactory');
+      await hre.run('verify:verify', {
+        address: ProxyFactory.address,
+        constructorArguments: [Api3ServerV1.address],
+      });
+
+      if (chainsSupportedByOevRelay.includes(network.name)) {
+        const usdcAddress = { ethereum: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48' };
+        const PrepaymentDepository = await deployments.get('PrepaymentDepository');
+        await hre.run('verify:verify', {
+          address: PrepaymentDepository.address,
+          constructorArguments: [
+            AccessControlRegistry.address,
+            'PrepaymentDepository admin (OEV Relay)',
+            OwnableCallForwarder.address,
+            usdcAddress[hre.network.name],
+          ],
+        });
+      }
+
+      if (chainsSupportedByApi3Market.includes(network.name)) {
+        const OrderPayable = await deployments.get('OrderPayable');
+        await hre.run('verify:verify', {
+          address: OrderPayable.address,
+          constructorArguments: [
+            AccessControlRegistry.address,
+            'OrderPayable admin (API3 Market)',
+            OwnableCallForwarder.address,
+          ],
+        });
+      }
+    }
+    if (chainsSupportedByChainApi.includes(network.name) || network.name === 'ethereum-sepolia-testnet') {
+      const RequesterAuthorizerWithErc721 = await deployments.get('RequesterAuthorizerWithErc721');
+      await hre.run('verify:verify', {
+        address: RequesterAuthorizerWithErc721.address,
+        constructorArguments: [AccessControlRegistry.address, 'RequesterAuthorizerWithErc721 admin'],
+      });
+    }
+  } else {
+    throw new Error(`${network.name} is not supported`);
   }
 };
 module.exports.tags = ['verify'];
