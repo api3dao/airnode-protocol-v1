d59f522497d35220b651b4d5b64d3e074fa1ad1f
bbenligiray <bbenligiray@gmail.com>
2023-06-12 11:15:40 +0300

    Increase MockErc20PermitToken supply

--- contracts/utils/mock/MockErc20PermitToken.sol
+++ contracts/utils/mock/MockErc20PermitToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract MockErc20PermitToken is ERC20Permit {
     constructor() ERC20("Token", "TKN") ERC20Permit("Token") {
-        _mint(msg.sender, 1e6 * 10 ** decimals());
+        _mint(msg.sender, 1e9 * 10 ** decimals());
     }
 
     function decimals() public view virtual override returns (uint8) {
