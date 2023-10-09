011bbabbc0a6caa037ff514752809f38747ae192
Santiago Aceñolaza <santiago@api3.org>
2023-10-09 16:48:30 -0300

    Adds natspec comments to TimestampedHashRegistry

--- contracts/utils/TimestampedHashRegistry.sol
+++ contracts/utils/TimestampedHashRegistry.sol
@@ -8,6 +8,18 @@ import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./interfaces/ITimestampedHashRegistry.sol";
 
+/// @title Contract that allows users to manage hashes by type which have been
+/// signed by a set of pre-defined signer accounts
+/// @notice This is intented to be a generic hash registry. These hashes must be
+/// signed by all the signers of a specific hash type. The signatures are
+/// validated and checked at the time a call to register the hash is made.
+/// This contract enables uses cases like adding data to merkle tree and then
+/// registering the root previously singed by a set of trusted accounts. Other
+/// contracts can then use the data sent to them only if a root of the merkle tree
+/// has been registered in this contract.
+/// @dev This contract inherits SelfMulticall meaning that all external functions
+/// can be called via multicall() or tryMulticall(). Hashes are expected to be
+/// signed following the EIP-712 signature specification.
 contract TimestampedHashRegistry is
     Ownable,
     EIP712,
@@ -19,8 +31,9 @@ contract TimestampedHashRegistry is
 
     mapping(bytes32 => EnumerableSet.AddressSet) private _hashTypeToSigners;
 
+    /// @notice Hashes by type (i.e. a merkle tree root, etc)
     mapping(bytes32 => bytes32) public hashTypeToHash;
-
+    /// @notice Timestamps representing when each hash was signed
     mapping(bytes32 => uint256) public hashTypeToTimestamp;
 
     bytes32 private constant _SIGNED_HASH_TYPE_HASH =
@@ -30,6 +43,9 @@ contract TimestampedHashRegistry is
 
     constructor() EIP712("TimestampedHashRegistry", "1.0.0") {}
 
+    /// @notice Called by the owner to set the hash signers
+    /// @param hashType Hash representing a hash type
+    /// @param signers Hash signers
     function setupSigners(
         bytes32 hashType,
         address[] calldata signers
@@ -45,6 +61,9 @@ contract TimestampedHashRegistry is
         emit SetupSigners(hashType, signers);
     }
 
+    /// @notice Called privately to add a new signer to the set of addresses
+    /// @param hashType Hash representing a hash type
+    /// @param signer // Signer address
     function _addSigner(bytes32 hashType, address signer) private {
         require(hashType != bytes32(0), "Hash type is zero");
         require(signer != address(0), "Signer is zero");
@@ -54,6 +73,9 @@ contract TimestampedHashRegistry is
         );
     }
 
+    /// @notice Called by the owner to add a new signer to the address set
+    /// @param hashType Hash representing a hash type
+    /// @param signer // Signer address
     function addSigner(
         bytes32 hashType,
         address signer
@@ -62,6 +84,13 @@ contract TimestampedHashRegistry is
         emit AddedSigner(hashType, signer);
     }
 
+    /// @notice Called by the owner to remove a signer from the address set
+    /// @dev This operation might change the order in the AddressSet and this
+    /// must be considered when trying to register a new hash since signatures
+    /// are expected to be received in the same order of the signers stored in
+    /// the contract
+    /// @param hashType Hash representing a hash type
+    /// @param signer // Signer address
     function removeSigner(
         bytes32 hashType,
         address signer
@@ -75,12 +104,19 @@ contract TimestampedHashRegistry is
         emit RemovedSigner(hashType, signer);
     }
 
+    /// @notice Returns the signers that are required to sign the hash for a type
+    /// @param hashType Hash representing a hash type
     function getSigners(
         bytes32 hashType
     ) external view override returns (address[] memory signers) {
         signers = _hashTypeToSigners[hashType].values();
     }
 
+    /// @notice Called to register a new hash for a type
+    /// @param hashType Hash representing a hash type
+    /// @param hash Signed hash
+    /// @param timestamp Timestamp when the hash was signed
+    /// @param signatures Hash signatures
     function registerHash(
         bytes32 hashType,
         bytes32 hash,
